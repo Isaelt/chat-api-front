f2148a8260eba3bbee1f6e16613c5a91da6ebd98
Isael Terrones <i.terayo@gmail.com>
2023-10-19 23:51:15 -0600

    correccion msjs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import theme from "./theme/newTheme";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
